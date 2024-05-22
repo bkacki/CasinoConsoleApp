85c05b948df0dfd1d26ec28e67808697bf800060
Bartek <bartlomiej.kacki@gmail.com>
2024-05-22 19:11:29 +0200

    Dodaj pliki projektów.

---
+++ CasinoConsoleApp/Guy.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasinoConsoleApp
+{
+    internal class Guy
+    {
+        public string Name;
+        public int Cash;
+
+        public void WriteMyInfo()
+        {
+            Console.WriteLine(Name + " ma " + Cash + " zł.");
+        }
+
+        public int GiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine(Name + " mówi: " + amount + " nie jest poprawną kwotą.");
+                return 0;
+            }
+            if (amount > Cash)
+            {
+                Console.WriteLine(Name + " mówi: " + "nie mam wystarczających środków, żeby dać Ci " + amount + " zł.");
+                return 0;
+            }
+            Cash -= amount;
+            return amount;
+        }
+
+        public void ReceiveCash(int amount)
+        {
+            if (amount <= 0)
+                Console.WriteLine(Name + " mówi: " + "nie przyjmę " + amount + " zł.");
+            else
+                Cash += amount;
+        }
+
+    }
+
+}
---
+++ CasinoConsoleApp/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace CasinoConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            double odds = .75;
+            Guy player = new Guy() { Cash = 100, Name = "Gracz" };
+
+            Console.WriteLine("Witaj w kasynie! Prawdopodobieństwo przegranej wynosi 0.75");
+
+            while(player.Cash > 0)
+            {
+                player.WriteMyInfo();
+
+                Console.Write("Podaj stawkę: ");
+                string howMuch = Console.ReadLine();
+
+                if (int.TryParse(howMuch, out int pot))
+                {
+                    if (pot > player.Cash)
+                    {
+                        Console.WriteLine("Niestety, nie masz tylu pieniędzy.");
+                        continue;
+                    }
+                    else
+                        pot *= 2;
+                }
+                else
+                {
+                    Console.WriteLine("Nie udało się przyjąć zakładu.");
+                    continue;
+                }
+
+                double bet = random.NextDouble();
+                if (bet > odds)
+                {
+                    player.Cash += pot;
+                    Console.WriteLine("Wygrałeś " + pot + " zł.");
+                }
+                else
+                {
+                    player.Cash -= pot/2;
+                    Console.WriteLine("Niestety, przegrałeś.");
+                }
+            }
+        }
+    }
+}
